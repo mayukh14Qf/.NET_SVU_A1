4709f94cfb67ff90d53bfa93abd8b4305e177f15
ABC <mayukc55@gmail.com>
2024-06-26 12:44:53 +0530

    day 2

--- Day2.cs
+++ Day2.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BCA_A1_207
 {
@@ -10,7 +6,66 @@ namespace BCA_A1_207
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World day 2");
+            // This is the Day 2 of C#
+
+            // variable ( it is container which contains the value)
+
+            // Data Types in C# (data types use as identification of Variable)
+
+            // (data Type)(Variable)(operator)(value);
+
+            // Integer value (For Integer Value)
+
+            int number1 = 96;
+            int numberTwo = 109;
+
+            // Float (for Decimal Value)
+
+            float numberThree = 12.89F;
+
+            // Double
+
+            double numberFour = 45.56D;
+
+            // Character (Only a single Character can be stored)
+
+            char char1 = 'f';
+
+            // String (a Sequence OF Characters)
+
+            string sentence1 = "My name is Mayukhjit";
+            string sentence2 = "w";
+            string sentence3 = "apple";
+            string sentence4 = "!@#$";
+
+
+            // Boolean (Only true or False)
+
+            bool check1=true;
+            bool check2=false;
+
+            // For Printng Purpose 
+            
+                        Console.WriteLine(number1);
+                        Console.WriteLine(numberTwo); 
+                        Console.WriteLine(numberThree);
+                        Console.WriteLine(numberFour);
+                        Console.WriteLine(char1);
+                        Console.WriteLine(sentence1);
+                        Console.WriteLine(sentence2);
+                        Console.WriteLine(sentence3);
+                        Console.WriteLine(sentence4);
+                        Console.WriteLine(check1);
+                        Console.WriteLine(check2);
+
+
+            // Difference Between Write and WriteLine            
+
+            Console.WriteLine("hello :((");
+            Console.Write("in the next Line");
+            Console.WriteLine(" Mayukhjit");
+            Console.Write("Chakraborty");
+
         }
     }
 }
