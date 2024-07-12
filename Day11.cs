ae29d64e3bba9eca5df229184750b7e7b2a80465
ABC <mayukc55@gmail.com>
2024-07-12 20:40:08 +0530

    day 12

--- Day11.cs
+++ Day11.cs
@@ -20,50 +20,50 @@ namespace BCA_A1_207
 
             // printing 1-100 in forward manner
 
-            //for (int mayukh = 1; mayukh <= 100; mayukh++) 
-            //{
-            //    Console.WriteLine(mayukh+" I am Sorry");
-            //}
+            for (int mayukh = 1; mayukh <= 100; mayukh++)
+            {
+                Console.WriteLine(mayukh + " I am Sorry");
+            }
 
             // Printing 1-100 in reverse order
 
-            //for (int i=100; i>=1; i--)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
 
             // Printing all even numbers within the range of 1 to 100
 
-            //for(int i=1; i<=100; i++)
-            //{
-            //    if(i % 2 == 0)
-            //    {
-            //        Console.WriteLine("Even : "+ i);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("odd : " + i);
-            //    }
-            //}
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Even : " + i);
+                }
+                else
+                {
+                    Console.WriteLine("odd : " + i);
+                }
+            }
 
             // or
             Console.WriteLine("Even Numbers");
-            //for (int i = 2; i <=100; i=i+2)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            for (int i = 2; i <= 100; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
 
-            //Console.WriteLine("Odd Numbers");
+            Console.WriteLine("Odd Numbers");
 
-            //for (int i = 1; i <= 100; i = i + 2)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            for (int i = 1; i <= 100; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
 
             // Print all the numbers Divisible by 7 within
             // the range of 25 to 600
 
-            for(int i = 25; i <= 600; i++)
+            for (int i = 25; i <= 600; i++)
             {
                 if (i % 7 == 0)
                 {
---
+++ Day12.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCA_A1_207
+{
+    public class Day12
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //factorial of a number
+
+            Console.WriteLine("enter a range:");
+            int range = Convert.ToInt32(Console.ReadLine());
+
+            int fact = 1;
+
+            for (int i = 1; i <= range; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine("factorial of the number is: " + fact);
+
+
+
+
+            //input a range from user find out
+            //print if the number is even and greater than 10
+
+            Console.WriteLine("enter range:");
+            int range2 = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= range2; i++)
+            {
+                if (i % 2 == 0 && i > 10)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+
+
+
+            //Count and find out factors of a number
+
+            Console.Write("enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int count2 = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count2++;
+                    Console.WriteLine(i); //It Will be Printing All the factors
+                }
+
+            }
+            Console.WriteLine("the number of factors: " + count2);
+
+
+
+        }
+
+    }
+}
