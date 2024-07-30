deadb8987fe57eff601b5e891729d8356fa69b64
ABC <mayukc55@gmail.com>
2024-07-30 18:31:32 +0530

    day 23

--- Day23.cs
+++ Day23.cs
@@ -9,7 +9,9 @@ namespace BCA_A1_207
     public class Day23
     {
         static void Main(String[] args)
-        {
+        { 
+            
+
             // Print only the prime digits of a number
 
             Console.WriteLine("Enter A number : ");
@@ -18,7 +20,7 @@ namespace BCA_A1_207
             Console.WriteLine("Prime digits are : ");
             while (num1 > 0)
             {
-                if(num1%10==2 || num1 % 10 == 3 || num1 % 10 == 5 || num1 % 10 == 2)
+                if(num1%10==2 || num1 % 10 == 3 || num1 % 10 == 5 || num1 % 10 == 7 )
                 {
                     Console.WriteLine(num1%10);
                 }
@@ -59,23 +61,38 @@ namespace BCA_A1_207
             }
 
 
+             
+
             // Print the series 
             // 5,8, 13 , 20, 29 .......
 
             Console.WriteLine("Enter Your Range : ");
             int position= Convert.ToInt32(Console.ReadLine());
 
-            int num = 5;
-            int gap = 3;
+            //int num = 5;
+            //int gap = 3;
 
-            for(int i = 1;i <= position; i++)
+            //for(int i = 1;i <= position; i++)
+            //{
+            //    Console.WriteLine(num);
+            //    num = num + gap;
+            //    gap = gap + 2;
+            //}
+
+            // or (method 2)
+
+            for(int i = 1; i <= position; i++)
             {
-                Console.WriteLine(num);
-                num = num + gap;
-                gap = gap + 2;
+                Console.Write(((i * i) + 4) + "  ");
             }
 
-            // Twin Prime
+            Console.WriteLine() ;
+
+
+
+
+
+            // Twin Prime within a range
 
             Console.WriteLine("Enter Your Range : ");
             int range2=Convert.ToInt32(Console.ReadLine());
@@ -105,6 +122,126 @@ namespace BCA_A1_207
                         Console.WriteLine("( " + i + " , " + (i + 2) + " )");
                     }
                 }
+            }
+
+
+
+
+
+            // 3,12,27,48 ..........
+           
+            Console.WriteLine("Enter Your Range : ");
+            int position1 = Convert.ToInt32(Console.ReadLine());
+
+            int answer = 0;
+            int counter = 1;
+
+            for(int i = 1;i <= position1; i++)
+            {
+                answer = answer + (3 * counter);
+                Console.WriteLine(answer);
+                counter = counter + 2;
+            }
+
+
+
+
+            /*
+             1 2 3 4 5
+             5 4 3 2 1
+             1 2 3 4 5
+             5 4 3 2 1
+             */
+
+            Console.WriteLine("Enter Your Number of rows : ");
+            int rows3 = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= rows3; i++)
+            {
+                if(i%2 == 0)
+                {
+                  
+                    for (int j = 5; j >= 1; j--)
+                    {
+                        Console.Write(j + " ");
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j <= 5; j++)
+                    {
+                        Console.Write(j + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+
+            // Find The Max And Min Digit of A Number
+
+            Console.WriteLine("Enter Your Number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int min = 9;
+            int max = 0;
+
+            while (num > 0)
+            {
+                min = Math.Min(min, num % 10);
+                max = Math.Max(max, num % 10);
+                num1 = num / 10;
+            }
+
+            Console.WriteLine("Min is " + min);
+            Console.WriteLine("Max is " + max);
+
+
+
+            // Diamond Pattern 
+
+            Console.WriteLine("Enter Your Number of rows : ");
+            int rows4 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows4; i++)
+            {
+                for (int j = rows4; j > i; j--)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+            for (int i = 1; i < rows4; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int j = rows4; j > i; j--)
+                {
+                    Console.Write("* ");
+                }
+
+                for (int j = rows4; j > i+1; j--)
+                {
+                    Console.Write("* ");
+                }
+
+
+                Console.WriteLine();
             }
 
         }
